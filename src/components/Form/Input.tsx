21e0ad6405e96af2469c1b238a8809d759c8c2a4
Gabriel Desidério <gabrieldesd7@gmail.com>
2022-05-05 18:40:43 -0300

    Validate forms

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,14 +1,27 @@
-import { FormControl, FormLabel, Input as CharkaInput, InputProps as ChakraInputProps } from '@chakra-ui/react'
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input as CharkaInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
   return (
-    <FormControl>
-      { !!label && <FormLabel htmlFor={name}>{ label }</FormLabel>}
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <CharkaInput
         name={name}
         id={name}
@@ -20,8 +33,17 @@ export function Input({ name, label, ...rest }: InputProps) {
           bgColor: "gray.900",
         }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+
+      { !!error && (
+        <FormErrorMessage>
+          { error.message }
+        </FormErrorMessage>
+      )}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
